e9a969242ae8eb6d2b5c0423e4b733af26212161
orianapresacan <55833012+orianapresacan@users.noreply.github.com>
2020-11-09 13:30:56 +0200

    6b

---
+++ src/main/java/com/park/parkinglot/common/CarDetails.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.parkinglot.common;
+
+/**
+ *
+ * @author Oriana
+ */
+public class CarDetails implements java.io.Serializable{
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    private Integer id;
+    private String licensePlate;
+    private String parkingSpot;
+    private String username;
+    
+    public CarDetails(Integer id, String licensePlate, String parkingSpot, String username){
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.parkingSpot = parkingSpot;
+        this.username = username;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/park/parkinglot/ejb/CarBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.parkinglot.ejb;
+
+import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.entity.Car;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Oriana
+ */
+@Stateless
+public class CarBean {
+
+    private static final Logger LOG = Logger.getLogger(CarBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+
+    private List<CarDetails> copyCarsToDetails(List<Car> cars) {
+        List<CarDetails> detailList = new ArrayList<>();
+        for (Car car : cars) {
+            CarDetails carDetails = new CarDetails(car.getId(),
+                    car.getLicensePlate(),
+                    car.getParkingSpot(),
+                    car.getUser().getusername());
+            detailList.add(carDetails);
+        }
+        return detailList;
+    }
+    public List<CarDetails> getAllCars() {
+        LOG.info("getAllCars");
+        try {
+            Query query = em.createQuery("SELECT c FROM Car c");
+            List<Car> cars = (List<Car>) query.getResultList();
+            return copyCarsToDetails(cars);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+
+}
