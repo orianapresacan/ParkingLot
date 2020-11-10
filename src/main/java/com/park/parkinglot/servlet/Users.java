b36a104ef0ae660aec2a88de2622644a1ce55ac6
orianapresacan <55833012+orianapresacan@users.noreply.github.com>
2020-11-10 12:00:39 +0200

    7b

--- src/main/java/com/park/parkinglot/servlet/Users.java
+++ src/main/java/com/park/parkinglot/servlet/Users.java
@@ -6,7 +6,9 @@
 package com.park.parkinglot.servlet;
 
 import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.common.UserDetails;
 import com.park.parkinglot.ejb.CarBean;
+import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -21,11 +23,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Oriana
  */
-@WebServlet(name = "Cars", urlPatterns = {"/Cars"})
-public class Cars extends HttpServlet {
+@WebServlet(name = "Users", urlPatterns = {"/Users"})
+public class Users extends HttpServlet {
 
     @Inject
-    private CarBean carBean;
+    private UserBean userBean;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -65,10 +67,10 @@ public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<CarDetails> cars = carBean.getAllCars();
-        request.setAttribute("cars", cars);
-        request.setAttribute("numberOfFreeParkingSpots", 10);
-        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
+        List<UserDetails> users = userBean.getAllUsers();
+        request.setAttribute("users", users);
+        request.setAttribute("numberOfAdmins", 1);
+        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
 
     }
 
